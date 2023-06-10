6a34bafd2bbe9107381b8fc9b8705c73e937a624
dafinalika <likadafina15@yahoo.com>
2023-06-10 12:22:31 +0200

    feat(auth) - created a module for auth - created a login/register page as component WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from 'src/auth/pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "auth", component: LoginComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
