0853ac6ed6b60971c6d919a0e6db25594eede890
alibeaj.flavio <alibeaj.flavio@gmail.com>
2023-06-10 14:45:52 +0200

    fix empty path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: "", redirectTo: "/home", pathMatch: "full" },
+  // { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: "home", loadChildren: () => import("./home-page/home-page.module").then(m => m.HomePageModule) }
 ];
 
