5bf99874b54473422b571a65cdc84e351e2c5eb6
dafinalika <likadafina15@yahoo.com>
2023-06-24 10:48:17 +0200

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from 'src/auth/pages/login/login.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { SingleBookPageComponent } from './single-book-page/single-book-page.component';
+
 
 const routes: Routes = [
   // { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: "home", component: HomePageComponent },
-  { path: "auth", component: LoginComponent }
+  { path: "auth", component: LoginComponent },
+  { path: "books/:id", component: SingleBookPageComponent },
 ];
 
 @NgModule({
